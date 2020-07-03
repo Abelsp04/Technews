4b086ab6746642c533ccfc543041d256cf100660
Abel Segui <Abelsegui@hotmail.com>
2020-07-03 21:30:53 +0000

    one component

--- src/js/index.js
+++ src/js/index.js
@@ -15,12 +15,12 @@ import { Home } from "./component/home.js";
 //render your react application
 
 ReactDOM.render(<Home />, document.querySelector("#app"));
-// document.querySelector(".menu-btn").addEventListener("click", () => {
-// 	document.querySelector(".nav-menu").classList.toggle("show");
-// });
+document.querySelector(".menu-btn").addEventListener("click", () => {
+	document.querySelector(".nav-menu").classList.toggle("show");
+});
 
-// ScrollReveal().reveal(".showcase-container");
-// ScrollReveal().reveal(".news-cards", { delay: 500 });
-// ScrollReveal().reveal(".news-cards-two", { delay: 500 });
-// ScrollReveal().reveal(".cards-banner", { delay: 500 });
-// ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
+ScrollReveal().reveal(".showcase-container");
+ScrollReveal().reveal(".news-cards", { delay: 500 });
+ScrollReveal().reveal(".news-cards-two", { delay: 500 });
+ScrollReveal().reveal(".cards-banner", { delay: 500 });
+ScrollReveal().reveal(".cards-banner-two", { delay: 500 });
