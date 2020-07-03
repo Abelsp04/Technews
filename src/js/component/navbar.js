077562774916b7c1d03ef082f30ef2a1e882a21a
Abel Segui <Abelsegui@hotmail.com>
2020-07-03 03:52:33 +0000

    component_built

---
+++ src/js/component/navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include images into your bundle
+//create your first component
+export function Home() {
+	return (
+		<div>
+			<div className="menu-btn">
+				<i className="fas fa-bars" />
+			</div>
+
+			<div className="container">
+				<nav className="nav-main">
+					<img
+						src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ1GQcxOAPMgAWadniqodkkefnJdgigYeyPhnBNMTrWUVFwPm_i&usqp=CAU"
+						alt="techNews Logo"
+						className="nav-brand"
+					/>
+					<ul className="nav-menu show ">
+						<li>
+							<a href="#">Website</a>
+						</li>
+						<li>
+							<a href="#">Block Chain</a>
+						</li>
+						<li>
+							<a href="#">Artifitial Intelligence</a>
+						</li>
+						<li>
+							<a href="#">Machine Learning</a>
+						</li>
+						<li>
+							<a href="#">Quantum Computing</a>
+						</li>
+						<li>
+							<a href="#">Startup</a>
+						</li>
+						<li>
+							<a href="#">More</a>
+						</li>
+					</ul>
+					<ul className="nav-menu-right">
+						<li>
+							<a href="#">
+								<i className="fas fa-search" />
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+			<hr />
+            </div>
+            )}
---
+++ src/js/component/showcase.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//include images into your bundle
+//create your first component
+export function Home() {
+	return (
+	
+		
+			<div className="showcase-container">
+				<h2>Big News Today</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+					do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					Duis aute irure dolor in reprehenderit in voluptate velit
+					esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia
+					deserunt mollit anim id est laborum.
+				</p>
+				<a href="#" className="btn">
+					Read More <i className="fas fa-angle-double-right" />
+				</a>
+            </div>
+            )}
